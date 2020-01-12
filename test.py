4a34d4c207d1c8d31fef3b2e11c5a9316aaa5431
HarishaRajGowda <harishrajgowda@hotmail.com>
2020-01-12 14:24:42 +0000

    Second Commit

--- test.py
+++ test.py
@@ -1,4 +1,11 @@
 import ipaddress
 subnet = ipaddress.IPv4Network('192.168.100.0/27');
 for ipv4 in subnet.hosts():
-    print(ipv4)
+    iprint(ipv4)
+
+a = 100
+b = 200
+if a > b:
+    print('{} is Greater than {}'.format(a,b))
+else:
+    print('{} is Greater than {}'.format(b,a))
